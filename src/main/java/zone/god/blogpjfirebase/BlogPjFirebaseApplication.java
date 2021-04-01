a2b714acd16453da5f9079dbf89987c01e6f9813
Z-One <trannhatthanh31@gmail.com>
2021-04-01 22:40:12 +0700

    First Commit. Added FireBase Starter.

---
+++ src/main/java/zone/god/blogpjfirebase/BlogPjFirebaseApplication.java
@@ -0,0 +1,13 @@
+package zone.god.blogpjfirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPjFirebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPjFirebaseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/zone/god/blogpjfirebase/config/FirebaseRealtime.java
@@ -0,0 +1,33 @@
+package zone.god.blogpjfirebase.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+
+@Service
+public class FirebaseRealtime {
+    @Value("${firebase.db.url}")
+    private String dbUrl;
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            File file = new ClassPathResource("static/blog-pj-fbver-firebase-adminsdk-bv1tj-0de3cafced.json").getFile();
+            FileInputStream account = new FileInputStream(file);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(account))
+                    .setDatabaseUrl(dbUrl)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
